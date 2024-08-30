cdc456c3d069eebe104d6a6487ba3358ca610576
Ansell Maximilian <ansellmaximilian@gmail.com>
2024-08-31 06:13:19 +0700

    remove unused auth helpers

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,24 +3,6 @@
 import { auth } from "@/auth";
 import { redirect } from "next/navigation";
 
-export const authenticated = async () => {
-  const session = await auth();
-
-  if (!session?.user) {
-    return redirect("/auth/login");
-  }
-
-  return session.user;
-};
-
-export const unAuthenticated = async () => {
-  const session = await auth();
-
-  if (session?.user) {
-    return redirect("/");
-  }
-};
-
 export class Auth {
   static async authenticated(redirectTo: string = "/auth/login") {
     "use server";
